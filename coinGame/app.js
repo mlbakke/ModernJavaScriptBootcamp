44a6f4f1a5cb44e1a95a385f8c915207aab7fffe
Martin Bakke <bakkemartin11@gmail.com>
2020-07-12 17:30:49 +0200

    added moveCoin logic
    finished demo

--- coinGame/app.js
+++ coinGame/app.js
@@ -1,7 +1,7 @@
 const player = document.querySelector('#player');
 const coin = document.querySelector('#coin');
 
-function isTouch(a, b) {
+function isTouching(a, b) {
 	const aRect = a.getBoundingClientRect();
 	const bRect = b.getBoundingClientRect();
 
@@ -40,9 +40,25 @@ window.addEventListener('keyup', function(e) {
 			player.style.left = `${currLeft + 50}px`;
 			break;
 	}
+	if (isTouching(player, coin)) moveCoin();
 });
 
 const extractPos = (pos) => {
 	if (!pos) return 10;
 	return parseInt(pos.slice(0, -2));
 };
+
+const moveCoin = () => {
+	let y = Math.floor(Math.random() * window.innerHeight);
+	let x = Math.floor(Math.random() * window.innerWidth);
+	if (y > window.innerHeight - 100) {
+		y -= 100;
+	}
+	if (x > window.innerWidth - 100) {
+		x -= 100;
+	}
+	coin.style.top = `${y}px`;
+	coin.style.left = `${x}px`;
+};
+
+moveCoin();
