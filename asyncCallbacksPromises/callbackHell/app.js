e2a520fc1be5a9ea1e64155b675d477577209442
Martin Bakke <bakkemartin11@gmail.com>
2020-07-21 16:34:08 +0200

    callback hell - comments

--- asyncCallbacksPromises/callbackHell/app.js
+++ asyncCallbacksPromises/callbackHell/app.js
@@ -6,9 +6,11 @@ const moveX = (element, amount, delay, onSuccess, onFail) => {
 	const currLeft = element.getBoundingClientRect().left;
 
 	setTimeout(() => {
+		// If trying to move off-screen, run fail function
 		if (elRight + amount > screenWidth) {
 			onFail();
 		} else {
+			// If there's room to move, move and run success function
 			element.style.transform = `translateX(${currLeft + amount}px)`;
 			onSuccess();
 		}
@@ -20,29 +22,35 @@ moveX(
 	300,
 	1000,
 	() => {
+		//On success, run again (*4, total 5*300px jumps)
 		moveX(
 			btn,
 			300,
 			1000,
 			() => {
 				moveX(
+					//On success, run again
 					btn,
 					300,
 					1000,
 					() => {
 						moveX(
+							//On success, run again
 							btn,
 							300,
 							1000,
 							() => {
 								moveX(
+									//On success, run again
 									btn,
 									300,
 									1000,
 									() => {
+										//On last success, congrats
 										alert('Cool screen, dude');
 									},
 									() => {
+										// If not successful, alert
 										alert('You are out of room');
 									}
 								);
