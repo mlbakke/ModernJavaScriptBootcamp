a53dc5bf66313cada193f29d6f48c7653ee8f7e6
Martin Bakke <bakkemartin11@gmail.com>
2020-07-29 16:40:28 +0200

    chained XMLHttpRequests

--- HTTPRequests/XMLHttpRequests/script.js
+++ HTTPRequests/XMLHttpRequests/script.js
@@ -1,11 +1,25 @@
 const firstReq = new XMLHttpRequest();
 
 firstReq.addEventListener('load', function() {
+	console.log('first request worked');
 	const data = JSON.parse(this.responseText);
+	const filmURL = data.results[0].films[0];
 
-	for (let planet of data.results) {
-		console.log(planet.name);
-	}
+	const filmReq = new XMLHttpRequest();
+	filmReq.addEventListener('load', function() {
+		console.log('film request worked');
+		const filmData = JSON.parse(this.responseText);
+		console.log(filmData);
+	});
+	filmReq.addEventListener('error', function(e) {
+		console.error('Error!!', e);
+	});
+	filmReq.open('GET', filmURL);
+	filmReq.send();
+
+	// for (let planet of data.results) {
+	// 	console.log(planet.name);
+	// }
 });
 firstReq.addEventListener('error', () => {
 	console.error('Error!!');
