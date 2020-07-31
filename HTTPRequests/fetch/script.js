cfca5390294b6356b797ad3dace68e3e9cc9dd74
Martin Bakke <bakkemartin11@gmail.com>
2020-07-31 16:33:00 +0200

    chained fetch requests

--- HTTPRequests/fetch/script.js
+++ HTTPRequests/fetch/script.js
@@ -4,11 +4,27 @@ const prom = fetch('https://swapi.dev/api/planets/')
 			// throw error -> .catch runs with this as argument
 			throw new Error(`Status code error: ${response.status}`);
 		}
-		response.json().then((data) => {
-			for (let planet of data.results) {
-				console.log(planet.name);
-			}
-		});
+		// Return promise:
+		// avoids having to nest chained promises
+		return response.json();
+	})
+	.then((data) => {
+		console.log('Fetched planets');
+		const filmUrl = data.results[0].films[0];
+		// fetch - promise
+		// can now add another .then
+		return fetch(filmUrl);
+	})
+	.then((response) => {
+		if (!response.ok) {
+			throw new Error(`Status code error: ${response.status}`);
+		}
+		// parse filmURL
+		return response.json();
+	})
+	.then((data) => {
+		console.log('Fetched first film, based off of first planet');
+		console.log(data.title);
 	})
 	.catch((err) => {
 		console.error('Fetch problem', err);
