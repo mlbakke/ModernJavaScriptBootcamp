f177ac348473a8ea348701e347a9231d6e82a154
Martin Bakke <bakkemartin11@gmail.com>
2020-08-03 18:53:23 +0200

    axios intro

--- HTTPRequests/axios/script.js
+++ HTTPRequests/axios/script.js
@@ -2,7 +2,7 @@
 //  1. don't have to parse JSON - it does it automatically
 //  2. Don't have to weed out bad status codes - it catches it automatically
 axios
-	.get('https://swapi.dev/api/planetsfeafea/')
+	.get('https://swapi.dev/api/planets/')
 	.then((res) => {
 		console.log(res.data);
 	})
