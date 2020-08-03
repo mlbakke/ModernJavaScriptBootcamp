74c5a6ec019fe233cf802a03595417ec59f8df57
Martin Bakke <bakkemartin11@gmail.com>
2020-08-03 17:25:24 +0200

    refactored fetch chains

--- HTTPRequests/fetch/script.js
+++ HTTPRequests/fetch/script.js
@@ -1,31 +1,40 @@
-const prom = fetch('https://swapi.dev/api/planets/')
-	.then((response) => {
-		if (!response.ok) {
-			// throw error -> .catch runs with this as argument
-			throw new Error(`Status code error: ${response.status}`);
-		}
-		// Return promise:
-		// avoids having to nest chained promises
-		return response.json();
-	})
-	.then((data) => {
-		console.log('Fetched planets');
-		const filmUrl = data.results[0].films[0];
-		// fetch - promise
-		// can now add another .then
-		return fetch(filmUrl);
-	})
-	.then((response) => {
-		if (!response.ok) {
-			throw new Error(`Status code error: ${response.status}`);
-		}
-		// parse filmURL
-		return response.json();
-	})
-	.then((data) => {
-		console.log('Fetched first film, based off of first planet');
-		console.log(data.title);
-	})
+const checkStatusAndParse = (response) => {
+	if (!response.ok) {
+		// throw error -> .catch runs with this as argument
+		throw new Error(`Status code error: ${response.status}`);
+	}
+	// Return promise:
+	// avoids having to nest chained promises
+	return response.json();
+};
+
+const printPlanets = (data) => {
+	console.log('%cLoaded 10 planets', 'color: rgb(0, 124, 0)');
+	for (let planet of data.results) {
+		console.log(planet.name);
+	}
+	// Return promise to keep chaining promises
+	return Promise.resolve(data.next);
+};
+
+const fetchNextPlanets = (url = 'https://swapi.dev/api/planets/') => {
+	// fetch = promise - can now add another .then
+	// 	looking into the next page from our API
+	// First page set as default for first time promise runs
+	return fetch(url);
+};
+
+// Fetches first three pages of planets, we could keep chaining
+//	to fetch more planets if we wanted
+fetchNextPlanets()
+	.then(checkStatusAndParse)
+	.then(printPlanets)
+	.then(fetchNextPlanets)
+	.then(checkStatusAndParse)
+	.then(printPlanets)
+	.then(fetchNextPlanets)
+	.then(checkStatusAndParse)
+	.then(printPlanets)
 	.catch((err) => {
 		console.error('Fetch problem', err);
 	});
