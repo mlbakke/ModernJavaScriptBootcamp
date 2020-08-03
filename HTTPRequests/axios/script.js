ce787b9e8f08a4448190759b2c64739a3ff9e87e
Martin Bakke <bakkemartin11@gmail.com>
2020-08-03 19:07:36 +0200

    axios chained requests

--- HTTPRequests/axios/script.js
+++ HTTPRequests/axios/script.js
@@ -1,11 +1,24 @@
+const fetchNextPlanets = (url = 'https://swapi.dev/api/planets/') => {
+	return axios.get(url);
+};
+
+const printPlanets = ({ data }) => {
+	console.log('%cLoaded 10 planets', 'color: rgb(0, 124, 0)');
+	for (let planet of data.results) {
+		console.log(planet.name);
+	}
+	return Promise.resolve(data.next);
+};
+
 // With axios you:
 //  1. don't have to parse JSON - it does it automatically
 //  2. Don't have to weed out bad status codes - it catches it automatically
-axios
-	.get('https://swapi.dev/api/planets/')
-	.then((res) => {
-		console.log(res.data);
-	})
+fetchNextPlanets()
+	.then(printPlanets)
+	.then(fetchNextPlanets)
+	.then(printPlanets)
+	.then(fetchNextPlanets)
+	.then(printPlanets)
 	.catch((err) => {
 		console.log(err);
 	});
