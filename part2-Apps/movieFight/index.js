9bc8e6ed296d3eae32256c87c45f019c91f2b33c
Martin Bakke <bakkemartin11@gmail.com>
2020-08-25 15:06:56 +0200

    fetching data set up

--- part2-Apps/movieFight/index.js
+++ part2-Apps/movieFight/index.js
@@ -1,1 +1,9 @@
-console.log('Hi there!');
+const fetchData = async () => {
+	const response = await axios.get('http://www.omdbapi.com/', {
+		params : {
+			apikey : '565268d9',
+			s      : 'avengers'
+		}
+	});
+	console.log(response.data);
+};
