9c5a38c0255f0201974362f04c5f3d558475412d
Martin Bakke <bakkemartin11@gmail.com>
2020-09-07 16:54:57 +0200

    added search function

--- part2-Apps/movieFight/index.js
+++ part2-Apps/movieFight/index.js
@@ -1,9 +1,24 @@
-const fetchData = async () => {
+const fetchData = async (searchTerm) => {
 	const response = await axios.get('http://www.omdbapi.com/', {
 		params : {
 			apikey : '565268d9',
-			s      : 'avengers'
+			s      : searchTerm
 		}
 	});
 	console.log(response.data);
 };
+
+const input = document.querySelector('input');
+let timeoutId;
+
+const onInput = (e) => {
+	if (timeoutId) {
+		clearTimeout(timeoutId);
+	}
+
+	timeoutId = setTimeout(() => {
+		fetchData(e.target.value);
+	}, 750);
+};
+
+input.addEventListener('input', onInput);
