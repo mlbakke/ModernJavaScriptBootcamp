c4a6dcd53e7a15c7ad5ef99172a9c1076ea2f68a
Martin Bakke <bakkemartin11@gmail.com>
2020-09-09 15:30:39 +0200

    Allow for two autocompletes
    Side by side
    
    Separate autocomplete script from index

---
+++ part2-Apps/movieFight/autocomplete.js
@@ -0,0 +1,60 @@
+const createAutocomplete = ({ root }) => {
+	root.innerHTML = `
+        <label><b>Search for a Movie</b></label>
+        <input class="input">
+        <div class="dropdown">
+            <div class="dropdown-menu">
+                <div class="dropdown-content results"></div>
+            </div>
+        </div>
+    `;
+
+	const input = root.querySelector('.input');
+	const dropdown = root.querySelector('.dropdown');
+	const resultsWrap = root.querySelector('.results');
+
+	const onInput = async (e) => {
+		const movies = await fetchData(e.target.value);
+		//Remove dropdown when input is empty
+		if (e.target.value === '') {
+			dropdown.classList.remove('is-active');
+			return;
+		}
+		//Clear previous results
+		resultsWrap.innerHTML = '';
+		//Open dropdown and add search results
+		dropdown.classList.add('is-active');
+		//If no results
+		if (!movies.length) {
+			resultsWrap.innerHTML = 'Sorry, no results found.';
+			return;
+		}
+		//If results
+		for (let movie of movies) {
+			const option = document.createElement('a');
+			const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
+			option.classList.add('dropdown-item');
+			option.innerHTML = `
+			<img src="${imgSrc}">
+			${movie.Title}
+		`;
+			option.addEventListener('click', () => {
+				dropdown.classList.remove('is-active');
+				input.value = movie.Title;
+				onMovieSelect(movie);
+			});
+
+			resultsWrap.appendChild(option);
+		}
+	};
+
+	// Search for movies on input, .5s delay
+	input.addEventListener('input', debounce(onInput, 500));
+
+	// Remove dropdown when clicking outside of it
+	document.addEventListener('click', (e) => {
+		if (!root.contains(e.target)) {
+			dropdown.classList.remove('is-active');
+		}
+	});
+};
--- part2-Apps/movieFight/index.js
+++ part2-Apps/movieFight/index.js
@@ -13,64 +13,14 @@ const fetchData = async (searchTerm) => {
 	return response.data.Search;
 };
 
-const root = document.querySelector('.autocomplete');
-root.innerHTML = `
-	<label><b>Search for a Movie</b></label>
-	<input class="input">
-	<div class="dropdown">
-		<div class="dropdown-menu">
-			<div class="dropdown-content results"></div>
-		</div>
-	</div>
-`;
-
-const input = document.querySelector('.input');
-const dropdown = document.querySelector('.dropdown');
-const resultsWrap = document.querySelector('.results');
-
-const onInput = async (e) => {
-	const movies = await fetchData(e.target.value);
-	//Remove dropdown when input is empty
-	if (e.target.value === '') {
-		dropdown.classList.remove('is-active');
-		return;
-	}
-	//Clear previous results
-	resultsWrap.innerHTML = '';
-	//Open dropdown and add search results
-	dropdown.classList.add('is-active');
-	//If no results
-	if (!movies.length) {
-		resultsWrap.innerHTML = 'Sorry, no results found.';
-		return;
-	}
-	//If results
-	for (let movie of movies) {
-		const option = document.createElement('a');
-		const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
-		option.classList.add('dropdown-item');
-		option.innerHTML = `
-			<img src="${imgSrc}" class="poster">
-			${movie.Title}
-		`;
-		option.addEventListener('click', () => {
-			dropdown.classList.remove('is-active');
-			input.value = movie.Title;
-			onMovieSelect(movie);
-		});
-
-		resultsWrap.appendChild(option);
-	}
-};
-
-// Search for movies on input, .5s delay
-input.addEventListener('input', debounce(onInput, 500));
-
-// Remove dropdown when clicking outside of it
-document.addEventListener('click', (e) => {
-	if (!root.contains(e.target)) {
-		dropdown.classList.remove('is-active');
-	}
+createAutocomplete({
+	root : document.querySelector('.autocomplete')
+});
+createAutocomplete({
+	root : document.querySelector('.autocomplete-2')
+});
+createAutocomplete({
+	root : document.querySelector('.autocomplete-3')
 });
 
 const onMovieSelect = async (movie) => {
