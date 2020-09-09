f8017c4f278ac680b833b1cd2cc6bd7cf3fc7dd9
Martin Bakke <bakkemartin11@gmail.com>
2020-09-09 13:09:22 +0200

    remove 'is-active' when clicking outside of it

--- part2-Apps/movieFight/index.js
+++ part2-Apps/movieFight/index.js
@@ -50,4 +50,8 @@ const onInput = async (e) => {
 
 input.addEventListener('input', debounce(onInput, 500));
 
-document.addEventListener('click', (e) => {});
+document.addEventListener('click', (e) => {
+	if (!root.contains(e.target)) {
+		dropdown.classList.remove('is-active');
+	}
+});
