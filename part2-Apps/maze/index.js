4cea1c7408db8add8fb611ca1077e5649b54a760
mlbakke <Martin.Bakke@student.uib.no>
2020-09-11 00:11:53 +0200

    maze: shape demo (temp)

--- part2-Apps/maze/index.js
+++ part2-Apps/maze/index.js
@@ -1,4 +1,7 @@
-const { Engine, Render, Runner, World, Bodies } = Matter;
+const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse } = Matter;
+
+const width = 800;
+const height = 600;
 
 const engine = Engine.create();
 const { world } = engine;
@@ -6,10 +9,44 @@ const render = Render.create({
     element: document.body,
     engine: engine,
     options: {
-        width: 800,
-        height: 600
+        wireframes: false,
+        width: width,
+        height: height
     }
 });
 
 Render.run(render);
 Runner.run(Runner.create(), engine);
+
+World.add(world, MouseConstraint.create(engine, {
+    mouse: Mouse.create(render.canvas)
+}));
+
+const walls = [
+    Bodies.rectangle((width/2), 0, width, 40, { isStatic: true}),
+    Bodies.rectangle((width/2), height, width, 40, { isStatic: true}),
+    Bodies.rectangle(0, (height/2), 40, height, { isStatic: true}),
+    Bodies.rectangle(width, (height/2), 40, height, { isStatic: true})
+];
+World.add(world, walls);
+
+//random shapes
+for (let i = 0; i < 25; i++) {
+    if (Math.random() > 0.5) {
+        World.add( world, 
+            Bodies.circle(Math.random() * width, Math.random() * height, 30, {
+                render: {
+                    fillStyle: 'rgb(110, 210, 70)'
+                }
+            })
+        );
+    } else {
+        World.add( world, 
+            Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50, {
+                render: {
+                    fillStyle: 'rgb(222, 55, 53)'
+                }
+            })
+        );
+    }
+};
