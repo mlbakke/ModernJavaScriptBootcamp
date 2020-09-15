4a289ead93fcc9a620706a25eb4d85339252115a
Martin Bakke <bakkemartin11@gmail.com>
2020-09-15 16:06:56 +0200

    created grid arrays

--- part2-Apps/maze/index.js
+++ part2-Apps/maze/index.js
@@ -1,52 +1,45 @@
 const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse } = Matter;
 
-const width = 800;
+const width = 600;
 const height = 600;
+
+const gridCols = 5; //number of columns in maze-grid
+const gridRows = 5; //number of rows in maze-grid
 
 const engine = Engine.create();
 const { world } = engine;
 const render = Render.create({
-    element: document.body,
-    engine: engine,
-    options: {
-        wireframes: false,
-        width: width,
-        height: height
-    }
+	element : document.body,
+	engine  : engine,
+	options : {
+		wireframes : true,
+		width      : width,
+		height     : height
+	}
 });
 
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-World.add(world, MouseConstraint.create(engine, {
-    mouse: Mouse.create(render.canvas)
-}));
+World.add(
+	world,
+	MouseConstraint.create(engine, {
+		mouse : Mouse.create(render.canvas)
+	})
+);
 
+// Borders
 const walls = [
-    Bodies.rectangle((width/2), 0, width, 40, { isStatic: true}),
-    Bodies.rectangle((width/2), height, width, 40, { isStatic: true}),
-    Bodies.rectangle(0, (height/2), 40, height, { isStatic: true}),
-    Bodies.rectangle(width, (height/2), 40, height, { isStatic: true})
+	Bodies.rectangle(width / 2, 0, width, 25, { isStatic: true }),
+	Bodies.rectangle(width / 2, height, width, 25, { isStatic: true }),
+	Bodies.rectangle(0, height / 2, 25, height, { isStatic: true }),
+	Bodies.rectangle(width, height / 2, 25, height, { isStatic: true })
 ];
 World.add(world, walls);
 
-//random shapes
-for (let i = 0; i < 25; i++) {
-    if (Math.random() > 0.5) {
-        World.add( world, 
-            Bodies.circle(Math.random() * width, Math.random() * height, 30, {
-                render: {
-                    fillStyle: 'rgb(110, 210, 70)'
-                }
-            })
-        );
-    } else {
-        World.add( world, 
-            Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50, {
-                render: {
-                    fillStyle: 'rgb(222, 55, 53)'
-                }
-            })
-        );
-    }
-};
+// Maze generation
+// Create grid
+const grid = Array(gridRows).fill(null).map(() => Array(gridCols).fill(false));
+// Vertical & horizontal grid lines
+const verticals = Array(gridRows).fill(null).map(() => Array(gridCols - 1).fill(false));
+const horizontals = Array(gridRows - 1).fill(null).map(() => Array(gridCols).fill(false));
