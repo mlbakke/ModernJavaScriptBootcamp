8f3a5d2c78638165b524c7d1932b7c2ae8e38755
Martin Bakke <bakkemartin11@gmail.com>
2020-09-16 19:30:34 +0200

    maze generation
    created pseudo-code for maze generation algorithm

--- part2-Apps/maze/index.js
+++ part2-Apps/maze/index.js
@@ -3,8 +3,8 @@ const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse } = Matter
 const width = 600;
 const height = 600;
 
-const gridCols = 5; //number of columns in maze-grid
-const gridRows = 5; //number of rows in maze-grid
+const gridCols = 3; //number of columns in maze-grid
+const gridRows = 3; //number of rows in maze-grid
 
 const engine = Engine.create();
 const { world } = engine;
@@ -43,3 +43,20 @@ const grid = Array(gridRows).fill(null).map(() => Array(gridCols).fill(false));
 // Vertical & horizontal grid lines
 const verticals = Array(gridRows).fill(null).map(() => Array(gridCols - 1).fill(false));
 const horizontals = Array(gridRows - 1).fill(null).map(() => Array(gridCols).fill(false));
+
+//Starting place for deleting walls in maze-generation algorithm
+const startRow = Math.floor(Math.random() * gridRows);
+const startCol = Math.floor(Math.random() * gridCols);
+//Go from one cell to a neighbour in maze-generation algorithm
+const switchCell = (row, column) => {
+	// If I have visted the cell at [row, column], return
+	// Mark cell as visited
+	// Assemble randomly-ordered list of neighbours
+	// For each neighbour:
+	// -See if neighbour is out of bounds
+	// -See if neighbour is visited, is so continue to next neighbour
+	//Remove a wall from either horizontals or verticals
+	//Go to cell
+};
+
+switchCell(startRow, startCol);
